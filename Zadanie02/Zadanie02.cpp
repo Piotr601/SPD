3684cc9ff85b827b538bb555fa0af36231250e9a
Piotr601 <piotr.niedziolka601@gmail.com>
2021-03-23 23:04:06 +0100

    Wczytywanie z pliku

---
+++ Zadanie02/Zadanie02.cpp
@@ -0,0 +1,62 @@
+﻿#include <iostream>
+#include <fstream>
+#include <time.h>
+#include <ctime>
+
+using namespace std;
+
+// liczba zadan
+int n = 0; 
+
+// do obslugi pliku
+string s;
+ifstream plik;
+
+// struktura danych (wczytanych plikow)
+typedef struct dane {
+public:
+    int t;  // czas wykonania zadania
+    int w;  // współczynnik kary 
+    int r;  // żądany termin zakończenia zadania
+    int x;  // kolejnosc
+} dane;
+
+dane witi[20];
+
+// wczytywanie danych
+void Wczytywanie() 
+{
+    // Wczytywanie z pliku
+    plik.open("data.txt");
+    
+    while (s != "data.10")
+    {
+        plik >> s;
+    }
+    plik >> n;
+
+    for (int i = 0; i < n; i++)
+    {
+        witi[i].x = i;
+        plik >> witi[i].t >> witi[i].w >> witi[i].r;
+    }
+
+    plik.close();
+
+    // Wyswietlanie ilosci danych
+    cout << " >>> n = " << n << " <<<\n\n";
+
+    // Wyswietlenie procesow ( danych )
+    for (int i = 0; i < n; i++)
+    {
+        cout << witi[i].x << ") " << witi[i].t << " " << witi[i].w << " " << witi[i].r << " " << endl;
+    }
+}
+
+int main()
+{
+    Wczytywanie();
+    
+}
+
+
