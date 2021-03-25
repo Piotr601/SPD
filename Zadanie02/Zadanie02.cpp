fd67bd46f71f7f9d47912c64736f37872ad7f9ef
Piotr601 <piotr.niedziolka601@gmail.com>
2021-03-25 22:59:09 +0100

    v2

--- Zadanie02/Zadanie02.cpp
+++ Zadanie02/Zadanie02.cpp
@@ -2,11 +2,13 @@
 #include <fstream>
 #include <time.h>
 #include <ctime>
+#include <algorithm>
 
 using namespace std;
 
 // liczba zadan
 int n = 0; 
+int pomtime;
 
 // do obslugi pliku
 string s;
@@ -29,7 +31,7 @@ void Wczytywanie()
     // Wczytywanie z pliku
     plik.open("data.txt");
     
-    while (s != "data.10")
+    while (s != "data.20")
     {
         plik >> s;
     }
@@ -53,10 +55,81 @@ void Wczytywanie()
     }
 }
 
+void Algorytm()
+{
+    // pomocnicza liczaca ilosc operacji
+    int s = 0;
+
+    // zmienna przechowująca ilosc operacji w postaci
+    // poteg 2, np w przypadku 10 // 2^10
+    int bit = 1 << n; 
+
+    // zmienne dynamiczne
+    int* F = new int[bit];
+    
+    // Wyzerowanie pierwszego elementu tablicy dynamicznej
+    F[0] = 0;
+
+    // dla wszystkich bitów
+    for (int bitcount = 1; bitcount < bit; bitcount++)  // od 1 do 2^bit ( w przypadku 10 // 1023)
+    {
+        // liczenie ilosci rozwiązań
+        pomtime = 0;
+        for (int i = 0, b = 1; i < n; i++, b *= 2) // b = 1, 2, 4, 8, ..
+        {
+            if (bitcount & b)
+            {
+                pomtime += witi[i].t;
+            }
+        }
+
+        // ustawienie ostatniej wartosci na maksymalną
+        F[bitcount] = 999999;
+
+        for (int j = 0, b = 1; j < n; j++, b *= 2)
+        {
+            if (bitcount & b)
+            {
+                // wybranie mniejszej wartosci ( 99999, 
+                F[bitcount] = min( F[bitcount], F[bitcount - b] + witi[j].w * max( pomtime - witi[j].r, 0 ) ) ;
+
+                //if ( F[bitcount] == F[bit - 1] )
+                //{
+                    //witi[j].x = F[bitcount - b];
+                    //cout << witi[j].x << " ";
+                //}
+            }
+        }
+    }
+    
+    //cout << "\nIlosc operacji: " << bit << endl;
+    //cout << "Optymalne rozwiazanie: " << F[bit - 1] << endl;
+
+    delete[] F;
+}
+
 int main()
 {
+    // WCZYTYWANIE DANYCH
     Wczytywanie();
-    
+
+    // do mierzenia czasu
+    int ilosc_powt = 1000;
+    time_t start, stop;
+    double t = 0;
+
+    // ALGORYTM
+    for (int i = 0; i < ilosc_powt; i++)
+    {
+        start = clock();
+        Algorytm();
+        stop = clock();
+
+        t += (double_t)(stop - start) / CLOCKS_PER_SEC;
+    }
+
+    // CZAS WYKONANIA SAMEGO ALGORYTMU
+    cout << "\nCzas wykonania: " << t/ilosc_powt << endl;
 }
 
 
